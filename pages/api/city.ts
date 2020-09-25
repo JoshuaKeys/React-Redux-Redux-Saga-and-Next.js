f495be8571e89ab4e04e44391fb3612a9d5e24d8
Joshua <joshua.oguma@outlook.com>
2020-09-25 09:12:36 +0300

    added functionality for adding cities

--- pages/api/city.ts
+++ pages/api/city.ts
@@ -8,13 +8,36 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case 'GET':
       return fetchCities(req, res, db);
+    case 'POST':
+      // Required Params - {city: string}
+      return addCity(req, res, db);
     default:
       res.status(404).json({ msg: "API not found" })
   }
 }
 
 async function fetchCities(req: NextApiRequest, res: NextApiResponse, db: Database) {
-  const cities = await db.all('SELECT * FROM city');
-  console.log(cities);
-  return res.json(cities)
+  try {
+    const cities = await db.all('SELECT * FROM city');
+    console.log(cities);
+    return res.json(cities)
+  } catch (err) {
+    res.status(500).send("Server Error")
+  }
+
+}
+async function addCity(req: NextApiRequest, res: NextApiResponse, db: Database) {
+  try {
+    const { city } = req.body;
+    if (!city) {
+      res.status(400).json({ err: 'Bad Request' })
+    }
+    console.log(city)
+    const insertOp = await db.run(`INSERT INTO City (name) values ('${city}') `);
+    const id = insertOp.lastID
+    return res.json({ id, city })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Could not insert into the database" })
+  }
 }
